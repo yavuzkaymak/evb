0f01d8d66f249270ed5f8396d7816135864b6789
kaymakyavuz <yavuzkaymak.asaf@gmail.com>
2022-12-28 23:45:16 +0100

    add log4j demo

---
+++ log4j_demo/src/main/java/de/otto/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package de.otto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	private String hellower(){
+		return "Hello Ahmet!";
+	}
+
+}
