5e4101fd5e83b164f3c72dd446b3e1f14e6402e6
kaymakyavuz <73892793+kaymakyavuz@users.noreply.github.com>
2022-12-31 22:32:06 +0100

    add vagrant version

--- vms/ubuntu/bootstrap.sh
+++ vms/ubuntu/bootstrap.sh
@@ -2,15 +2,15 @@
 
 install_docker() {
     step "===== Installing docker ====="
-    sudo apt update
-    sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    sudo apt-get update
+    sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     if [ $? -ne 0 ]; then
         sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     fi
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    sudo apt update
-    sudo apt install -y docker-ce docker-ce-cli containerd.io
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
     sudo groupadd docker
     sudo gpasswd -a $USER docker
     # Add vagrant to docker group
@@ -18,21 +18,21 @@ install_docker() {
     sudo gpasswd -a vagrant docker
     # Setup docker daemon host
     # Read more about docker daemon https://docs.docker.com/engine/reference/commandline/dockerd/
-    sed -i 's/ExecStart=.*/ExecStart=\/usr\/bin\/dockerd -H unix:\/\/\/var\/run\/docker.sock -H tcp:\/\/192.168.121.210/g' /lib/systemd/system/docker.service
     sudo systemctl daemon-reload
     sudo systemctl restart docker
 }
 
 install_openssh() {
     step "===== Installing openssh ====="
-    sudo apt update
-    sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common 
-    sudo apt install -y openssh-server
+    sudo apt-get -qq update
+    sudo apt-get -yq install apt-transport-https ca-certificates curl gnupg-agent software-properties-common 
+    sudo apt-get install -yq openssh-server
     sudo systemctl enable ssh
 }
 
 install_tools() {
-    sudo apt install -y python3 git 
+    sudo apt-get update -qq
+    sudo apt-get install -yq python3 git code-oss
 
 }
 
